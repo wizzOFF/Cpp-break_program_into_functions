7653b49dc46c12dcc121c324cf4b534d3e3439b5
WizzOFF <maher_off@proton.me>
2025-03-27 21:04:14 +0100

    break_program_into_functions.cpp: function returns nothing

--- break_program_into_functions.cpp
+++ break_program_into_functions.cpp
@@ -1,11 +1,27 @@
 
 #include <iostream>
 using namespace std;
-
+/*
 string demanderNom()
 {    
      cout << "Entrez votre nom : ";
      string nom;
      cin >> nom;
      return nom;
+}
+*/
+
+void direBonjour()
+{
+    cout << "Bonjour !" << endl;
+    //Comme rien ne ressort, il n'y a pas de return !
+}
+
+int main()
+{
+    direBonjour();
+    //Comme la fonction ne renvoie rien
+    //On l'appelle sans mettre la valeur de retour dans une variable
+    
+    return 0;
 }
