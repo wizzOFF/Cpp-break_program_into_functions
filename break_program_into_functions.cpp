71369d0eaa81f9d8851661a99d04188557d1a253
WizzOFF <maher_off@proton.me>
2025-03-27 21:18:53 +0100

    break_program_into_functions.cpp: practical case > two-arguments function

--- break_program_into_functions.cpp
+++ break_program_into_functions.cpp
@@ -46,7 +46,7 @@ int main()
     return 0;
 }
 */
-
+/*
 double carre(double x)
 {
     double nombre;
@@ -63,5 +63,30 @@ int main()
     carreNombre = carre(nombre); //On utilise la fonction
 
     cout << "Le carre de " << nombre << " est " << carreNombre << endl;
+    return 0;
+}
+*/
+
+void dessineRectangle(int l, int h)
+{
+    for(int ligne(0); ligne < h; ligne++)
+    {
+        for(int colonne(0); colonne < l; colonne++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    } 
+}
+
+int main()
+{
+    int largeur, hauteur;
+    cout << "Largeur du rectangle : ";
+    cin >> largeur;
+    cout << "Hauteur du rectangle : ";
+    cin >> hauteur;
+    
+    dessineRectangle(largeur, hauteur);
     return 0;
 }
